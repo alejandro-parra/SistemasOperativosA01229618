be719d8b7bccab0393ab6df73848bdf8f907514d
Alejandro Parra Altamirano <alexparra_07@hotmail.com>
2019-09-11 03:15:05 +0000

    llamadas a sistema

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -132,6 +132,8 @@ static int (*syscalls[])(void) = {
 [SYS_reboot] sys_reboot,
 };
 
+const char *Syscalls[23] = {"fork", "exit", "wait", "pipe", "read", "kill", "exec", "fstat", "chdir", "dup", "getpid", "sbrk", "sleep", "uptime", "open", "write", "mknod", "unlink", "link", "mkdir", "close", "shutdown", "reboot"};
+
 void
 syscall(void)
 {
@@ -139,6 +141,7 @@ syscall(void)
   struct proc *curproc = myproc();
 
   num = curproc->tf->eax;
+  cprintf("%s -> %d ", Syscalls[num], num + 1);
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
   } else {
